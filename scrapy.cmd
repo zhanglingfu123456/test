f7dd510e5c5cb5dc6078a6dadc525f06e124dbe6
Your Name <704921095@qq.com>
2018-01-03 20:26:08 +0800

    按照

---
+++ scrapy.cmd
@@ -0,0 +1,11 @@
+echo 99
+F:
+cd paper_spider/paper
+scrapy crawl paper
+scrapy crawl ai_hui_net
+scrapy crawl ai_zhi_xue_zhe
+scrapy crawl china_library
+scrapy crawl library_high_school
+scrapy crawl china_meeting_online
+exit
+
